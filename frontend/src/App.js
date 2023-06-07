ba6b2636ba6f62b82934f78d364988030ebcf676
Bakhtovar2004 <mamurovbakhtovar721@gmail.com>
2023-06-07 11:27:57 +0500

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-blue-900">
-        Hello Tailwind
+        Bakhtovar's changes
       </h1>
           </div>
   );
