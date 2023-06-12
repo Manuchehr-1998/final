e91efc831c9fc7dbd2d8b9d16cde238d2e72d260
Manuchehr <manuchehrsaidov998@gmail.com>
2023-06-12 10:20:12 +0500

    Nav Commit

--- frontend/src/components/slider/Slider.jsx
+++ frontend/src/components/slider/Slider.jsx
@@ -1,45 +1,88 @@
-import { Carousel } from "@material-tailwind/react";
-import foto from '../../img/slider.png'
-import foto1 from '../../img/stol2.jpeg'
-import foto2 from '../../img/drinks (6).jpg'
-
+import React, { useState, useEffect } from 'react';
+import foto from '../../img/slider.png';
+import foto1 from '../../img/stol2.jpeg';
+import foto2 from '../../img/drinks (6).jpg';
 
 export default function Slider() {
-    return (
-        <Carousel
-            navigation={({ setActiveIndex, activeIndex, length }) => (
-                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-                    {new Array(length).fill("").map((_, i) => (
-                        <span
-                            key={i}
-                            className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "bg-white w-8" : "bg-white/50 w-4"
-                                }`}
-                            onClick={() => setActiveIndex(i)}
-                        />
-                    ))}
-                </div>
-            )} >
-            <div>
-                <img src={foto} alt="image1" className="h-[660px] w-full object-cover" />
-                <h1 className="text-white absolute top-[200px] ml-[100px] w-[600px] italic font-normal text-5xl text-center transform -rotate-6">
-                    ДОСТАВКА ВКУСНЕЙШИХ БЛЮД ЗА 60 МИНУТ
-                </h1>
-                <button className=' bg-green-400 text-white absolute top-[360px] px-2 py-2  ml-[280px]  italic font-normal text-2xl text-center transform -rotate-6'>ЕЩЁ НЕ ПРОБОВАЛ?</button>
-            </div>
-            <div>
-                <img src={foto1} alt="image2" className="h-[660px] w-full object-cover" />
-                <h1 className="text-white absolute top-[200px] ml-[100px] w-[500px] italic font-normal text-5xl text-center transform -rotate-6">
-                    ЗАКАЗ СТОЛ ДАЖЕ СИДЯ ДОМА
-                </h1>
-                <button className='bg-green-400 text-white absolute top-[320px] px-2 py-2 ml-[250px]  italic font-normal text-2xl text-center transform -rotate-6'>ЗАКАЗ МОЖНО ЗДЕСЬ</button>
-            </div>
-            <div>
-                <img src={foto2} alt="image3" className="h-[660px] w-full object-cover" />
-                <h1 className="text-white absolute top-[200px] ml-[240px] w-[100px] italic font-normal text-5xl text-center transform -rotate-6">
-                    ХОЛОДНЫЕ НАПИТКИ
-                </h1>
-                <button className='bg-green-400 text-white absolute top-[320px] ml-[260px] px-2 py-2 italic font-normal text-2xl text-center transform -rotate-6'>ЕЩЁ НЕ ПРОБОВАЛ?</button>
-            </div>
-        </Carousel >
-    );
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNextSlide();
+    }, 4000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const handleSlideChange = (index) => {
+    setActiveIndex(index);
+  };
+
+  const handleNextSlide = () => {
+    const lastIndex = 2;
+    setActiveIndex((prevIndex) => (prevIndex === lastIndex ? 0 : prevIndex + 1));
+  };
+
+  const handlePrevSlide = () => {
+    const lastIndex = 2;
+    setActiveIndex((prevIndex) => (prevIndex === 0 ? lastIndex : prevIndex - 1));
+  };
+
+  return (
+    <div className="relative">
+      <img
+        src={activeIndex === 0 ? foto : activeIndex === 1 ? foto1 : foto2}
+        alt="slider"
+        className="h-[660px] w-full object-cover"
+      />
+      <div className="absolute top-4 left-4 flex items-center gap-4">
+        <button
+          className="bg-green-400 text-white px-2 py-1 rounded"
+          onClick={handlePrevSlide}
+        >
+          Prev
+        </button>
+      </div>
+      <div className="absolute top-4 right-4 flex items-center gap-4">
+        <button
+          className="bg-green-400 text-white px-2 py-1 rounded"
+          onClick={handleNextSlide}
+        >
+          Next
+        </button>
+      </div>
+      <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+        <span
+          className={`block h-1 cursor-pointer rounded-2xl transition-all bg-white/50 w-4 ${
+            activeIndex === 0 ? 'bg-white w-8' : ''
+          }`}
+          onClick={() => handleSlideChange(0)}
+        />
+        <span
+          className={`block h-1 cursor-pointer rounded-2xl transition-all bg-white/50 w-4 ${
+            activeIndex === 1 ? 'bg-white w-8' : ''
+          }`}
+          onClick={() => handleSlideChange(1)}
+        />
+        <span
+          className={`block h-1 cursor-pointer rounded-2xl transition-all bg-white/50 w-4 ${
+            activeIndex === 2 ? 'bg-white w-8' : ''
+          }`}
+          onClick={() => handleSlideChange(2)}
+        />
+      </div>
+      <h1 className="text-white absolute top-[200px] ml-[100px] w-[600px] italic font-normal text-5xl text-center transform -rotate-6">
+        {activeIndex === 0
+          ? 'ДОСТАВКА ВКУСНЕЙШИХ БЛЮД ЗА 60 МИНУТ'
+          : activeIndex === 1
+          ? 'ЗАКАЗ СТОЛ ДАЖЕ СИДЯ ДОМА'
+          : 'ХОЛОДНЫЕ НАПИТКИ'}
+      </h1>
+      <button className="bg-green-400 text-white absolute top-[360px] px-2 py-2 ml-[280px]  italic font-normal text-2xl text-center transform -rotate-6">
+        ЕЩЁ НЕ ПРОБОВАЛ?
+      </button>
+    </div>
+  );
 }
