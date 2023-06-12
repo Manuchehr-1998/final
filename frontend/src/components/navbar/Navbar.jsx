8854a51e8bbad432c8bcc9e54960adf0ba191d8c
Manuchehr <manuchehrsaidov998@gmail.com>
2023-06-12 10:54:21 +0500

    s

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,70 +1,85 @@
-import {
-    Button,
-    IconButton,
-    Input,
-} from "@material-tailwind/react";
-import logo from '../../img/LOGO.png'
-import { BiSearch } from 'react-icons/bi'
-import { IoCall } from 'react-icons/io5'
-import { useState } from "react";
-import ModalBasked from "./ModalBasked";
-export default function Navbar() {
-    const [modalOpen, setModalOpen] = useState(false);
+import React, { useState } from 'react';
+import logo from '../../img/LOGO.png';
+import { BiSearch } from 'react-icons/bi';
+import { IoCall } from 'react-icons/io5';
+import ModalBasked from './ModalBasked';
 
+const Navbar = () => {
+  const [modalOpen, setModalOpen] = useState(false);
 
-    const handleModalClick = () => {
-        setModalOpen(!modalOpen);
-    };
+  const handleModalClick = () => {
+    setModalOpen(!modalOpen);
+  };
 
-    return (
-        <div className="w-full bg-[#403C3B] px-4 py-3 h-[80px] flex items-center">
-            <div className="flex flex-wrap items-center justify-between gap-y-4 text-white">
-                <img src={logo} alt="Logos" className="w-[110px] h-[40px] ml-[96px]" />
-                <div className="relative flex w-full gap-2 md:w-max">
-                    <Input
-                        type="search"
-                        color="white"
-                        label="Поиск блюдо..."
-                        className="pr-20"
-                        containerProps={{
-                            className: "min-w-[450px] ml-[72px]",
-                        }}
-                    />
-                    <IconButton
-                        size="sm"
-                        className="!absolute right-1 top-1  bg-[#403C3B]"
-                    >
-                        <BiSearch />
-                    </IconButton>
-                </div>
-                <div
-                    className="ml-[30px] flex gap-3 items-center mr-[750px] ">
-                    <IconButton className="bg-green-400 rounded-full w-[32px] h-[32px]" >
-                        <IoCall />
-                    </IconButton>
-                    <div>
-                        <p>Контакты:</p>
-                        <a href="number">+7 (917) 510-57-59</a>
-                    </div>
-                </div>
+  return (
+    <div className="w-full bg-[#403C3B] px-4 py-3 h-20 flex items-center sm:w-full">
+      <div className="w-full flex justify-between items-center text-white">
+        <div className="flex items-center gap-20">
+          {/* Hamburger Menu */}
+          <div className="flex items-center sm:hidden">
+            <button className="text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
 
-                <div
-                    className="flex items-center bg-green-400 rounded-md  w-[150px] h-[52px] justify-center gap-3">
-                    <Button
-                        variant="text"
-                        color="white"
-                        onClick={handleModalClick}>
-                        <p>Корзина</p>
-                    </Button>
-                    {modalOpen && (
-                        <div className={`modal z-50 ${modalOpen ? "animate" : "" }`}>
-                            <ModalBasked/>
-                        </div>
-                    )}
-                    <div className="h-[38px] w-[1px] bg-white -ml-3"></div>
-                    <span className="rounded-full  bg-white text-black w-6 h-6 flex items-center px-2">0</span>
-                </div>
+          {/* Logo */}
+          <img src={logo} alt="Logo" className="w-28 ml-10 sm:ml-0" />
+
+          {/* Search Input */}
+          <div className="relative hidden sm:flex gap-2">
+            <input
+              type="search"
+              className="bg-gray-700 text-white px-4 py-2 w-[450px] rounded focus:outline-none focus:ring focus:border-green-300"
+              placeholder="Поиск блюда..."
+            />
+            <button className="absolute right-0 top-0 bg-gray-700 text-white px-2 pt-3 rounded">
+              <BiSearch />
+            </button>
+          </div>
+
+          {/* Contact */}
+          <div className="hidden sm:flex items-center">
+            <button className="bg-green-400 rounded-full w-8 h-8 flex items-center justify-center">
+              <IoCall />
+            </button>
+            <div className="ml-2">
+              <p>Контакты:</p>
+              <a href="tel:+79175105759">+7 (917) 510-57-59</a>
             </div>
+          </div>
         </div>
-    );
-}
+
+        {/* Cart */}
+        <div className="flex items-center gap-3 bg-green-400 p-2 rounded-lg">
+          <button className="text-white" onClick={handleModalClick}>
+            Корзина
+          </button>
+          {modalOpen && (
+            <div className={`modal z-50 ${modalOpen ? 'animate' : ''}`}>
+              <ModalBasked />
+            </div>
+          )}
+          <div className="h-8 w-0.5 bg-white hidden sm:block"></div>
+          <span className="rounded-full bg-white text-black w-4 h-4 flex items-center justify-center px-1">
+            0
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
