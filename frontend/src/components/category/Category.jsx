18e9047a0389a2d84f3baf111cdd433d4936757d
Manuchehr <manuchehrsaidov998@gmail.com>
2023-06-19 09:55:27 +0500

    Home-commit

--- frontend/src/components/category/Category.jsx
+++ frontend/src/components/category/Category.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Category = () => {
+  const categoryData = [
+    {
+      text: "Холодные закуски",
+    },
+    {
+      text: "Горячие закуски",
+    },
+    {
+      text: "Мясные блюда",
+    },
+    {
+      text: "Супы",
+    },
+    {
+      text: "Рыбные блюда",
+    },
+    {
+      text: "Гриль меню",
+    },
+    {
+      text: "Фирменные блюда",
+    },
+    {
+      text: "Напитки",
+    },
+  ];
   return (
-    <div>Category</div>
-  )
-}
+    <div className="flex justify-evenly w-[100%] h-[10vh] items-center bg-[#403C3B]">
+      {categoryData.map((item, index) => (
+    
+          <div key={index}>
+            <h6 className="text-[#CFCFCF]">{item.text}</h6>
+          <hr className="hover:bg-[#618967] w-[100%] hover:h-[4px] bg-none" />
+          </div>
+        
+      ))}
+      <hr />
+    </div>
+  );
+};
 
-export default Category
+export default Category;
--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
-const Footer = () => {
+import React from "react";
+import logo from "../../assets/LOGO.png";
+import { FaArrowUp } from "react-icons/fa";
+export default function Footer() {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-[#403C3B] flex items-center  w-full text-white gap-[100px] p-[2vh] xs:w-[100%] xs:p-[1vh] xs:flex xs:gap-10">
+      <div className=" rounded-full bg-white text-black ml-20 xs:ml-2">
+        <button className="p-4">
+          <FaArrowUp />
+        </button>
+      </div>
+      <div className="pt-10 pb-2 flex flex-col gap-2 xs:gap-1 ">
+        <img src={logo} className="w-[150px]" alt="" />
+        <p className="text-[12px]">© ООО СК «АПШЕРОН» <br /> Все права защищены. 2010-2020 </p>
 
-export default Footer
+        <div className="flex flex-col gap-2 text-[14px]">
+          <a href="a">Ползовательское соглашение</a>
+          <a href="a">Карта сайта</a>
+          <a href="a">Политика конфиденциальности</a>
+        </div>
+      </div>
+      <div className="flex pl-6 gap-11 text-[20px] xs:hidden">
+        <a href="a">О ресторане</a>
+        <a href="a">Условия доставки</a>
+        <a href="a">Возврат товара</a>
+        <a href="a">Акция</a>
+      </div>
+    </div>
+  );
+}
