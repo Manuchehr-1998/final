ce62eb8454a09740745c28101dd761503c1f6ad0
Manuchehr <manuchehrsaidov998@gmail.com>
2023-06-20 10:29:56 +0500

    modal-commit

--- frontend/src/components/Header/Modal.jsx
+++ frontend/src/components/Header/Modal.jsx
@@ -6,7 +6,7 @@ export default function ModalBasked() {
       <div className="xs:flex xs:flex-wrap xs:justify-center flex flex-wrap justify-center w-[100%] xs:w-[100%] xs:p-2">
       <img src={basket} alt=""  className=""/>
       <h1 className="text-4xl text-center  text-white xs:text-[20px]">КОРЗИНА ПУСТАЯ</h1>
-      <button className="bg-green-400 text-white w-[80%] p-2 rounded-lg mt-6 xs:p-2">
+      <button className="bg-green-400 text-white w-[100%] p-2 rounded-lg mt-6 xs:p-2">
         Посмотреть меню
       </button>
       </div>
