58945a6c4817b8ada872c7dafaf581954faf069c
Dilovar <tsdilovar@mail.ru>
2023-07-06 13:06:49 +0500

    добавления

--- backend/src/models/OrdersModel.ts
+++ backend/src/models/OrdersModel.ts
@@ -3,13 +3,13 @@ import { OrderHistoryTypes } from "../types";
 
 export class OrdersModel {
   // Send Orders
-  static async getOrders(): Promise<OrderHistoryTypes[]> {
-    const result = await pool.query("SELECT FROM orders");
+  static async getHistoryOfOrders(): Promise<OrderHistoryTypes[]> {
+    const result = await pool.query("SELECT FROM history_of_orders");
     return result.rows;
   }
 
-  // Receiving a new order
-  static async AddOrder(
+  // Adding an order to the order history
+  static async AddOrdertoHistory(
     street: string,
     comment: string,
     order: any,
@@ -17,8 +17,11 @@ export class OrdersModel {
     sum: number
   ) {
     try {
-      text: "INSERS INTO orders (street, comment, order, date, sum) VALUES ($1, $2, $3, $4, $5";
-      values: [street, comment, order, date, sum];
+      const query = {
+        text: "INSERS INTO history_of_orders (street, comment, order, date, sum) VALUES ($1, $2, $3, $4, $5)",
+        values: [street, comment, order, date, sum],
+      };
+      await pool.query(query);
     } catch (error) {
       console.error(error);
     }
